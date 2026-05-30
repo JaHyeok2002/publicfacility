fb22616c5bc34ea4fffcd691c0b29cebce9095c1
kujahyeok <jhku2433@gmail.com>
2026-05-30 21:02:28 +0900

    테스트 코드 커밋

---
+++ backend/src/test/java/com/example/publicfacility/PublicfacilityApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.publicfacility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicfacilityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
